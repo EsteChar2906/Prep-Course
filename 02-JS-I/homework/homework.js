25f3da7d52b6c2486bcad00a76de9a75da33ea75
EsteChar2906 <ccarlospachecoc@gmail.com>
2022-04-24 15:48:56 -0500

    Nuevo Editor de texto

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -149,7 +149,8 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  
+  var NombreCompleto = nombre + apellido;
+  return NombreCompleto;
 }
 
 function obtenerSaludo(nombre) {
