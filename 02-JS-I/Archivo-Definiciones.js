2da86ccd1af3bad24f8d81ed4ecef791e79cbca8
EsteChar2906 <ccarlospachecoc@gmail.com>
2022-04-24 18:58:15 -0500

    Archivo de texto

---
+++ 02-JS-I/Archivo-Definiciones.js
@@ -0,0 +1,37 @@
+const { calculateExtensionPriority } = require("@11ty/eleventy/src/TemplateData")
+const { normalize } = require("@11ty/eleventy/src/TemplatePath")
+const { stringify } = require("git-url-parse")
+const { tienenMismaLongitud } = require("./homework/homework")
+
+DEFINICIONES
+
+* Variables:
+Es una forma que te permite almacenar el valor de algo para luego utilizarlo.
+En otras palabras, una variable es algo en lo que puedes guardar el valor de algo, ya sea valor numerico,
+de caracteres (Sting) o que demarque una condicion. Por ejemplo: 
+Una variable puede ser cuantos años tienes y la respuesta de esta sera su valor: AñosdeEdad = 25, 
+en este caso es un valor numerico.
+Una casa es una variable y su color puede ser su valor (Casa: roja), en este caso valor (tipo de dato) string
+Una pregunta como Fumas?, tambien es una variable. Fumas = si o normalize. en este el valor es un dato Booleano (de condicion)
+ 
+* Strings:
+Los string son cadenas de caracteres que siempre se representan entre comillas. La definicion de un string no es nada mas 
+que un texto entre comillas por ejemplo, "hola". Pero un numero tambien puede ser un string si esta entre comillas, al estar en esta 
+condicion la consola no lo tomara como numero, ejemplo, "Hola 1 + 1"
+
+* Funciones (argumentos, `return`)
+La funciones son una parte esencial en todo lenguaje de programacion y se utilizan para calcular, cambiar o hacer algo con conjutos de variables. 
+Las funciones hacen parte de los keywords, por lo cual, tienen la condicion de se objetos intocables.
+
+Un argumento es una variable que se utiliza para incorporar valores en una funcion.
+
+Return el una keywords que ayuda a que los datos escapen de una funcion y se regresen a la variable anterior. Este nos permite comprobar condiciones
+y dar respuesta a la funcion segun el argumento ingresado.
+
+* Declaraciones `if`
+Una declaracion es aquella que establece una asociacion entre una variable, funcion o tiopo determinado y sus atibutos.
+if se usa para tomar decisiones lo que hace es evaluar la operacion logica o expresion que de como resultado la condicion que le indiquemos
+las mas generales son true y false, donde si se cumple true se ejecutara la linea declarada.
+
+* Valores booleanos (`true`, `false`)
+Estos valores provienen de la logica de Boole y generalmente demarcan una condicion y su contraparte. Es decir true es un valor booleano porque si no es true, es false.
